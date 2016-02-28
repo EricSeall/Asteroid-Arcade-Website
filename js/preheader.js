5a3d46c4033f9e8d581af8cf8ff2ba24fa9096f5
Eric Seall <ejseall@gmail.com>
2016-02-28 11:11:23 -0600

    Add files

---
+++ js/preheader.js
@@ -0,0 +1,34 @@
+var isAbout = false;
+var isContact = false;
+var isMembers = false;
+var isMusic = false;
+
+document.write(`
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+<meta name="description" content="The official Asteroid Arcade website.">
+<meta name="author" content="">
+<!-- Favicon -->
+<link rel="icon" type="image/png" href="media/logo.png" />
+<link rel="icon" type="image/x-icon" href="media/favicon.ico" />
+
+<!-- jQuery v. 2.2.0 -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+
+<!-- Bootstrap core JS v. 3.3.6 -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha256-KXn5puMvxCw+dAYznun+drMdG1IFl3agK0p/pqT9KAo= sha512-2e8qq0ETcfWRI4HJBzQiA3UoyFk6tbNyG+qSaIBZLyW9Xf3sWZHN/lxe9fTh1U45DpPf07yj94KsUHHWe4Yk1A==" crossorigin="anonymous"></script>
+
+<!-- Bootstrap core CSS v. 3.3.6 -->
+<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet" integrity="sha256-7s5uDGW3AHqw6xtJmNNtr+OBRJUlgkNJEo78P4b0yRw= sha512-nNo+yCHEyn0smMxSswnf/OnX6/KwJuZTlNZBjauKhTK0c+zT+q5JOCx0UFhXQ6rJR9jg6Es8gPuD2uZcYDLqSw==" crossorigin="anonymous">
+
+<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+<!--[if lt IE 9]>
+  <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+<![endif]-->
+
+<!-- Custom styles for this template -->
+<link href="css/style.css" rel="stylesheet">
+`);
